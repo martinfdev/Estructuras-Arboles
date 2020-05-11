5a2c733479d3733ada4951a4bfac237dac395f52
Pedro <pedromartinf07@gmail.com>
2020-05-11 12:51:22 -0600

    Peer to peer network

--- src/com/interfaces/Login.java
+++ src/com/interfaces/Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.interfaz;
+package com.interfaces;
 import com.structures.User;
 import com.structures.HashTable;
 import java.util.Arrays;
